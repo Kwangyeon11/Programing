7ad3d0f8d0a54a27be71f7925228d71d12a6523d
Kwangyeon11 <bnmliopa@gmail.com>
2025-09-10 15:29:33 +0900

    Update chap7_3.c

--- chap07/chap7_3.c
+++ chap07/chap7_3.c
@@ -1,4 +1,8 @@
-// 25. 07. 07
+/*   파일명: Assignment07_3.c
+	 내  용: PA02. 특정 값으로 초기화된 정수형 배열에 대하여 배열의 원소 중 최댓값과 최솟값을 찾아서 출력하는 프로그램을 작성하시오.
+	 작성자: 주광연
+	 날  짜: 2025.09. 14
+*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -35,15 +39,15 @@ void print_num()
 	int arr[] = { 23, 45, 62, 12, 99, 83, 23, 50, 72, 37 };
 	int size = sizeof(arr) / sizeof(arr[0]);
 
-	printf("�迭: ");
+	printf("¹è¿­: ");
 	for (int i = 0; i < size; i++)
 	{
 		printf("%d ", arr[i]);
 	}
 	printf("\n");
 
-	printf("�ִ�: %d\n", find_max(arr, size));
-	printf("�ִ�: %d\n", find_min(arr, size));
+	printf("ÃÖ´ñ°ª: %d\n", find_max(arr, size));
+	printf("ÃÖ´ñ°ª: %d\n", find_min(arr, size));
 }
 
 int main()
@@ -51,4 +55,5 @@ int main()
 	print_num();
 
 	return 0;
+
 }
