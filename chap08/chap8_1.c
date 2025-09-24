ec196787bc91664268fabd8287c82f7d06c585aa
Kwangyeon11 <bnmliopa@gmail.com>
2025-09-24 14:22:55 +0900

    Update and rename ch08_01.c to chap8_1.c

--- chap08/chap8_1.c
+++ chap08/chap8_1.c
@@ -1,8 +1,3 @@
-/*   파일명: ch08_01.c
-	 내  용: PA01. 크기가 3인 double형 배열의 모든 원소의 주소를 출력하는 프로그램을 작성하시오.(난이도 1)
-	 작성자: 주광연
-	 날  짜: 2025.09. 24
-*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -25,3 +20,4 @@ int main()
 	return 0;
 
 }
+
